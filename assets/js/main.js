6766002de4015c13a61363d8eb9daf41abda913b
Sebs-rss <seba.r.sanchez@gmail.com>
2024-08-11 16:57:54 -0400

    estructura base, solo faltan los tooltip

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+
+/*Smooth scroll*/
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({block: "end", behavior: 'smooth' });
+}
+
+
