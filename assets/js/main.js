f1e1bcdff46f5884cbeded1afe26e06935aeb2d6
Sebs-rss <seba.r.sanchez@gmail.com>
2024-08-12 22:39:28 -0400

    Mejoras en formulario y tooltips básicos (sin estilo bootstrap)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 
-/*Smooth scroll*/
+//Smooth scroll
 function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({block: "end", behavior: 'smooth' });
 }
 
+//Tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 
