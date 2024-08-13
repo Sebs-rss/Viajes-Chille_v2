6a9af88747bb8acc186c20bc5a6eb355cf18be32
Sebs-rss <seba.r.sanchez@gmail.com>
2024-08-13 17:10:39 -0400

    mejoras a tooltips

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 //Smooth scroll
 function scrollToSection(sectionId) {
